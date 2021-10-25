f44281f934f3639127fdcdf3a36b8fdd606f07d4
serjukl <serjukl98@gmail.com>
2021-10-25 19:48:12 +0300

    fix

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-// var req = new XMLHttpRequest();  
-// req.open('GET', 'http://www.mydomain.com/', false);   
-// req.send(null);  
-// if(req.status == 200) {
-//     console.log(req.responseText)
-//    dump(req.responseText);
-// }
-
-const url = 'https://news.ycombinator.com/'
-
-axios.get(url)
-    .then(response => {
-        console.log(response.data)
-    })
-    .catch(error => {
-        console.log(error);
-    })
+var request = require('request');
+var URL = 'http://www.ferra.ru/ru/techlife/news/';
+request(URL, function (err, res, body) {
+    if (err) throw err;
+    console.log(body);
+    console.log(res.statusCode);
+});
