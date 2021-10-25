518de0408e2d94f5e77f8042299e50ad52038574
serjukl <serjukl98@gmail.com>
2021-10-25 19:39:53 +0300

    fix

---
+++ index.js
@@ -0,0 +1,17 @@
+// var req = new XMLHttpRequest();  
+// req.open('GET', 'http://www.mydomain.com/', false);   
+// req.send(null);  
+// if(req.status == 200) {
+//     console.log(req.responseText)
+//    dump(req.responseText);
+// }
+
+const url = 'http://www.mydomain.com'
+
+axios.get(url)
+    .then(response => {
+        console.log(response.data)
+    })
+    .catch(error => {
+        console.log(error);
+    })
