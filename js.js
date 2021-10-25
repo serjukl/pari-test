bcbe7b45bb9830c0979da00c635724e4670a614f
serjukl <serjukl98@gmail.com>
2021-10-25 19:35:12 +0300

    fix

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 var req = new XMLHttpRequest();  
-req.open('GET', 'https://www.mydomain.com/', false);   
+req.open('GET', 'http://www.mydomain.com/', false);   
 req.send(null);  
 if(req.status == 200) {
     console.log(req.responseText)
